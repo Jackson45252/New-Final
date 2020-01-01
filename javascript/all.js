e333dc56eeaffa813fb7dff7812670f333ae4eb7
sbsy1517 <sbsy1517@gmail.com>
2020-01-01 22:06:11 +0800

    section#about

---
+++ javascript/all.js
@@ -0,0 +1,34 @@
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+  }
+  
+  /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+  }
+
+
+
+$(window).ready(function () {
+  $(window).scroll(function(evt){
+    if ($(window).scrollTop()>0)
+      $(".myNavbar").removeClass("navbar-top");
+    else
+      $(".myNavbar").addClass("navbar-top");
+  });
+});
+
+$(document).on("click",'.fas',function(e){
+  e.preventDefault();
+  let target=$('.fas').attr('href');
+  console.log(alert(target));
+  $('html,body').animate({
+    scrollTop: $(target).offset().top - 107.9
+  },500);
+});
+
+ 
+

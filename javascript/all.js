d0c129c915b85690b51ba97d3a5eee4fa3607a2a
sbsy1517 <sbsy1517@gmail.com>
2020-01-02 02:44:45 +0800

    fix

--- javascript/all.js
+++ javascript/all.js
@@ -1,34 +1,40 @@
-/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
-function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
-  }
+// /* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+// function openNav() {
+//     document.getElementById("mySidenav").style.width = "250px";
+//     document.getElementById("main").style.marginLeft = "250px";
+//   }
   
-  /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
-  }
+//   /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+//   function closeNav() {
+//     document.getElementById("mySidenav").style.width = "0";
+//     document.getElementById("main").style.marginLeft = "0";
+//   }
 
 
 
-$(window).ready(function () {
-  $(window).scroll(function(evt){
+
+$(document).ready(function(){
+  
+  $(window).scroll(function(e){
     if ($(window).scrollTop()>0)
       $(".myNavbar").removeClass("navbar-top");
     else
       $(".myNavbar").addClass("navbar-top");
   });
-});
+  // 
+  $(document).on("click",'.fas',function(e){
+    e.preventDefault();
+    let target=$('.fas').attr('href');
+    // console.log(alert(target));
+    $('html,body').animate({
+      scrollTop: $(target).offset().top - 140
+    },500);
+  });
+  $(window).stellar();
 
-$(document).on("click",'.fas',function(e){
-  e.preventDefault();
-  let target=$('.fas').attr('href');
-  // console.log(alert(target));
-  $('html,body').animate({
-    scrollTop: $(target).offset().top - 107.9
-  },500);
-});
+})
+
+
 
  
 
